d94f72e528475690c8dfc4643c9c4a7af0023b2e
Harrison Max-Wilson <H.MaxWilson@hotmail.com>
2023-09-17 22:39:06 +0100

    Initial commit

---
+++ src/components/global/TheFooter.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer class="bg-gray-900 text-white">
+    <div class="container mx-auto py-4 px-4">
+      <div class="flex flex-wrap items-center justify-between">
+        <div class="flex flex-wrap mx-auto sm:mx-0 text-sm justify-around font-medium leading-5">
+          <a href="/" class="px-3 py-1 hover:underline">Home</a>
+          <a href="/about" class="px-3 py-1 hover:underline">About</a>
+          <a href="/services" class="px-3 py-1 hover:underline">Services</a>
+        </div>
+        <div class="hidden sm:block text-sm">
+          Phone: <a class="opacity-80 hover:opacity-100" href="tel:07900890911">07562 763552</a> <br/>
+          Email: <a class="opacity-80 hover:opacity-100" href="mailto:admin@astronomad.co.uk">admin@astronomad.co.uk</a>
+        </div>
+      </div>
+      <div class="text-center py-2">
+        &copy; 2023 AstroNomad
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script setup>
+
+</script>
---
+++ src/components/global/TheHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="bg-gray-900 text-white">
+    <div class="container mx-auto py-4 px-4">
+      <div class="w-full flex col-span-1 sm:col-span-2 lg:col-span-2 justify-between">
+        <a href="/">
+          <h2 class="block text-2xl font-medium my-auto">AstroNomad Web Development</h2>
+        </a>
+        <nav class="my-auto">
+          <ul class="flex flex-wrap justify-center sm:justify-end text-sm font-medium leading-5">
+            <li class="px-3 py-1 hover:underline"><a href="/">Home</a></li>
+            <li class="px-3 py-1 hover:underline"><a href="/about">About</a></li>
+            <li class="px-3 py-1 hover:underline"><a href="/services">Services</a></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script setup>
+
+</script>
